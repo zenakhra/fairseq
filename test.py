6f0051e47e3b10679ab8133547570e08043ead07
zenakhra <29185414+zenakhra@users.noreply.github.com>
2022-02-03 00:11:25 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,17 +3,26 @@ import fairseq
 import torchaudio
 import numpy as np
 from torch import nn
+from os import walk
+from scipy import spatial
 
 model, cfg, task = fairseq.checkpoint_utils.load_model_ensemble_and_task(['wav2vec_large.pt'])
 model = model[0]
 model.eval()
-waveform, sample_rate = torchaudio.load('fairseq/cat/badri-1.wav')
-wav_input_16khz = torch.randn(1,10000)
-z = model.feature_extractor(wav_input_16khz)
-c = model.feature_aggregator(z)
-print("feature extractor : ")
-print(z.cpu().detach().numpy().shape)
-print(z.cpu().detach().numpy())
-print("feature aggregator : ")
-print(c.cpu().detach().numpy().shape)
-print(c.cpu().detach().numpy())
+for (dirpath, dirnames, filenames) in walk('fairseq/cat'):
+  for i in range(len(filenames)):
+    for j in range(i+1,len(filenames)):
+      print(filenames[i],filenames[j])
+      waveform1, sample_rate1 = torchaudio.load('fairseq/cat/'+filenames[i])
+      z1 = model.feature_extractor(waveform1)
+      c1 = model.feature_aggregator(z1)
+      feature1 = z1.cpu().detach().numpy()
+      print("feature aggregator : "+filenames[i])
+      print(feature1.shape)
+      waveform2, sample_rate1 = torchaudio.load('fairseq/cat/'+filenames[j])
+      z2 = model.feature_extractor(waveform2)
+      c2 = model.feature_aggregator(z2)
+      feature2 = z2.cpu().detach().numpy()
+      print("feature aggregator : "+filenames[j])
+      print(feature2.shape)
+      result = 1 - spatial.distance.cosine(feature1.flatten(), feature2.flatten())
